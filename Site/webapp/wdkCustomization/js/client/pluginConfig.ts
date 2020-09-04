de80f64e710eceb7a59b938cad1182af08ecc5b5
Jamie Long <jtlong3rd@gmail.com>
2020-09-04 12:15:22 -0400

    Initial commit of Phyletic Pattern summary view

--- Site/webapp/wdkCustomization/js/client/pluginConfig.ts
+++ Site/webapp/wdkCustomization/js/client/pluginConfig.ts
@@ -2,11 +2,18 @@ import { ClientPluginRegistryEntry } from 'wdk-client/Utils/ClientPlugin';
 
 import { Form as GroupsByPhyleticPatternForm } from '../questions/GroupsByPhyleticPattern/Form';
 
+import { PhyleticPattern } from 'ortho-client/results/PhyleticPattern';
+
 const orthoPluginConfig: ClientPluginRegistryEntry<any>[] = [
   {
     type: 'questionForm',
     searchName: 'GroupsByPhyleticPattern',
     component: GroupsByPhyleticPatternForm
+  },
+  {
+    type: 'summaryView',
+    name: 'phyletic',
+    component: PhyleticPattern
   }
 ];
 
---
+++ Site/webapp/wdkCustomization/js/client/results/PhyleticPattern.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+
+}
+
+export function PhyleticPattern(props: Props) {
+  return (
+    <div>
+      Future home of the Phyletic Pattern summary view
+    </div>
+  );
+}
